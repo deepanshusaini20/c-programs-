8375613be04dbed55f1b30caf551a8149cd495fe
Deepanshu Saini <96648990+deepanshusaini20@users.noreply.github.com>
2022-01-10 23:47:49 +0530

    Add files via upload

--- factorial using recursion/main.c
+++ factorial using recursion/main.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main(){
+    int r;
+    float multiply1,multiply2;
+    printf("\t----------------------------\n");
+    printf("radius:");
+    scanf("%d",&r);
+    multiply1=(float)3.14159265*r*r;
+    multiply2=(float)3.14159265*2*r;
+    printf("The area of circle is: %f \n",multiply1);
+    printf("The circumference of circle is: %f \n",multiply2);
+    return 0;
+    
+    
+}
