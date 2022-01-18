0febfa626af05571a932cdabba1f419e678f5360
Deepanshu Saini <96648990+deepanshusaini20@users.noreply.github.com>
2022-01-19 01:32:41 +0530

    Create year.c

---
+++ leap year/year.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a;
+    printf("\t ENTER THE YEAR:\n");
+    scanf("%d",&a);
+
+    //leap year is perfectly divisible by 400
+    if(a%400==0)
+    {
+        printf("IT'S A LEAP YEAR");
+    }
+
+    //not a leap year if divisible by 100 but not divisible by 400
+    else if(a%100==0)
+    {
+        printf("IT'S NOT A LEAP YEAR");
+    }
+    //leap year if not divisible by 100 but divisible by 4
+    else if(a%4==0){
+        printf("IT'S A LEAP YEAR");
+    }
+    else{
+        printf("IT'S NOT A LEAP YEAR");
+    }
+    return 0;
+
+}
