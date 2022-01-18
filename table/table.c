305310469f17633e6a307b49d4277ecb70744ea9
Deepanshu Saini <96648990+deepanshusaini20@users.noreply.github.com>
2022-01-19 02:00:35 +0530

    Update table.c

--- table/table.c
+++ table/table.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<math.h>
-#include<conio.h>
+#include<conio.h>//included for getch()
 int main(){
     int a,i,table=1;
-    printf("\tenter a number to get its table\n");
+    printf("\tenter a number to get its table\n");// taking input number from user to print it's table
     printf("\t-------------------------------\n");
     scanf("%d",&a);
     for(i=1;i<=10;i++){
         
-    table=a*i;
+    table=a*i;//loop is used to multiply the given number by it's increment(+1) upto 10 times 
     
-    printf("%d",table);
+    printf("%d",table);//, is used to get 3,6,9 between numbers in output 
     printf(",");
         
     }
