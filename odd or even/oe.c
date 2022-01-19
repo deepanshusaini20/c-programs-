d5c94bb55507432fa65ffbc750cfdc2c43693265
Deepanshu Saini <96648990+deepanshusaini20@users.noreply.github.com>
2022-01-19 10:41:22 +0530

    Update oe.c

--- odd or even/oe.c
+++ odd or even/oe.c
@@ -4,7 +4,7 @@ int main(){
     printf("\t Enter a number\n ");
     //taking value from user to be checked for odd or even
     scanf("%d",&a);
-    x=(a%2==0)?printf("The number is even"):printf("The number is odd");
+    x=(a%2==0)?printf("The number is even"):printf("The number is odd");//here ternary operator is used
     //number completely divisible by 2 is even else odd
     return 0;
 
