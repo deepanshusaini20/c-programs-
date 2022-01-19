3b73744d22bf693cf77195d9925a97b78fce6138
Deepanshu Saini <96648990+deepanshusaini20@users.noreply.github.com>
2022-01-19 20:29:14 +0530

    Update num.c

--- number swap/num.c
+++ number swap/num.c
@@ -2,7 +2,7 @@
 #include<math.h>
 int main (){
     int a,b;
-    printf("\tenter two values for a and b\n");
+    printf("\tenter two values for a and b\n");// taking two values from user to swap them
     printf("\t----------------------------\n");
     printf("a:");
     scanf("%d",&a);
@@ -11,6 +11,7 @@ int main (){
     a=a+b;
     b=a-b;
     a=a-b;
+    //mathematical operations to swap two numbers 
     printf("Numbers after swapping are:a=%d,b=%d",a,b);
     return 0;
     }
