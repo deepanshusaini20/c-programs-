b2badb18f90059e62de0b212a3c7cbebd5227357
Deepanshu Saini <96648990+deepanshusaini20@users.noreply.github.com>
2022-01-19 10:38:07 +0530

    Create oe.c

---
+++ odd or even/oe.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,x;
+    printf("\t Enter a number\n ");
+    //taking value from user to be checked for odd or even
+    scanf("%d",&a);
+    x=(a%2==0)?printf("The number is even"):printf("The number is odd");
+    //number completely divisible by 2 is even else odd
+    return 0;
+
+}
