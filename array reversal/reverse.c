cef0d47b93ec15aa1b7a9f12d69a4beea41e0ce1
Deepanshu Saini <96648990+deepanshusaini20@users.noreply.github.com>
2022-02-03 22:02:24 +0530

    Update reverse.c

--- array reversal/reverse.c
+++ array reversal/reverse.c
@@ -3,11 +3,13 @@
 int main(){
     int i;
     int a[10/2];
-    printf("\tenter values of array\n:");
+    printf("\tenter values of array\n");
     for(i=0;i<5;i++){
         scanf("%d",&a[i]);
     }
-    for(i=4;i>=0;i--){
+    printf("\n");
+    printf("reverse array: ");
+    for(i=4;i>=0;i--){  // initializing loop in decrement to get reverse of array
         printf("%d,",a[i]);
     }
 
